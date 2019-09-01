b69ae9d373aa0335709d892adbb4a4ea023073ba
Philip A Senger <philip.a.senger@cngrgroup.com>
2019-09-01 23:04:28 +1000

    * added iv to streams

--- src/utils/__test__/cryptoWrapper.spec.js
+++ src/utils/__test__/cryptoWrapper.spec.js
@@ -1,4 +1,7 @@
-const {encrypt,decrypt} = require('../cryptoWrapper');
+const {encrypt,decrypt,buildCipherStream,buildDecipherStream} = require('../cryptoWrapper');
+const fs = require('fs');
+const path = require('path');
+const concat = require('concat-stream');
 
 const randomNumber = ( start, end ) => {
     return Math.floor (Math.random() * ( Math.abs(end) - Math.abs(start)) + Math.abs(start) )
@@ -14,11 +17,12 @@ function randomString(numberOfChars, { sampleSet = 'abcdefghijklmnopqrstuvwxyzAB
 
 describe('cryptoWrapper',()=>{
     const key = randomString(32);
+    const salt = 'salty';
     const randomLetters = randomString(100);
     let encryptedString = '';
     describe('#encrypt',()=>{
         test('should encrypt without failing',()=>{
-            encryptedString = encrypt(randomLetters,key);
+            encryptedString = encrypt(randomLetters,key, salt);
             expect(encryptedString).not.toEqual('');
         })
         test('should fail due to invalid key length, 31', ()=>{
@@ -36,7 +40,7 @@ describe('cryptoWrapper',()=>{
     });
     describe('#decrypt',()=>{
         test('should decrypt without failing', ()=>{
-            const unEncryptedString = decrypt(encryptedString,key);
+            const unEncryptedString = decrypt(encryptedString,key,salt);
             expect(unEncryptedString).not.toEqual(encryptedString);
         })
         test('should fail due to invalid key length, 31', ()=>{
@@ -51,5 +55,24 @@ describe('cryptoWrapper',()=>{
         test('should fail due to invalid key length, null', ()=>{
             expect(() => decrypt(encryptedString,null)).toThrow();
         })
+    });
+    describe('#buildCipherStream and #buildDecipherStream',()=>{
+        const { cipherStream, ivStream } = buildCipherStream('abc123','saltysalt');
+        const deCipherStream = buildDecipherStream('abc123','saltysalt');
+        test('encrypted',(done)=>{
+            const org = fs.readFileSync(path.join(__dirname,'test.txt'), {encoding:'utf8'});
+            const InStream = fs.createReadStream(path.join(__dirname,'test.txt'), {encoding:'utf8'});
+            expect(() => {
+                const concatStream = concat({},(cmp)=>{
+                    expect( org ).toEqual( cmp.toString() );
+                    done();
+                });
+                InStream
+                    .pipe(cipherStream)
+                    .pipe(ivStream)
+                    .pipe(deCipherStream)
+                    .pipe(concatStream);
+            } ).not.toThrow();
+        });
     });
 })
--- src/utils/cryptoWrapper.js
+++ src/utils/cryptoWrapper.js
@@ -1,37 +1,89 @@
 const crypto = require('crypto');
+const { Transform } = require('stream');
 
-const IV_LENGTH = 16; // For AES, this is always 16
+// For AES, this is always 16
+const IV_LENGTH = 16;
 const ALGORITHM = 'aes-256-cbc';
+const buildKey = (password, salt) => Buffer.from(crypto.scryptSync(password, salt, 32));
 
-function encrypt(text, key) {
-    if ( key.length !== 32 ) throw new Error('AES Key Must be 256 bytes (32 characters)');
-    let iv = crypto.randomBytes(IV_LENGTH);
-    let cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(key), iv);
-    let encrypted = cipher.update(`${ ( Math.round ( ( new Date().getTime() * Math.random() ) / Math.random() ) ).toString(16) }\n${text}`);
+const encrypt = function encrypt(text, password, salt) {
+    const key = buildKey(password, salt);
+    let initializationVector = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(ALGORITHM, key, initializationVector);
+    let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
-}
+    return initializationVector.toString('hex') + ':' + encrypted.toString('hex');
+};
 
-function decrypt(text, key) {
-    if ( key.length !== 32 ) throw new Error('AES Key Must be 256 bytes (32 characters)');
+const decrypt = function decrypt(text, password, salt) {
+    const key = buildKey(password, salt);
     let textParts = text.split(':');
-    let iv = Buffer.from(textParts.shift(), 'hex');
+    // key and iv must be 'binary' encoded strings or buffers.
+    let initializationVector = Buffer.from(textParts.shift(), 'hex');
     let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(key), iv);
+    let decipher = crypto.createDecipheriv(ALGORITHM, key, initializationVector);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
-    const [date,...rest] =decrypted.toString().split('\n');
-    return rest.join('\n');
-}
+    return decrypted.toString();
+};
 
-function encryptStream(key) {
-    if ( key.length !== 32 ) throw new Error('AES Key Must be 256 bytes (32 characters)');
-    return crypto.createCipher(ALGORITHM, key);
-}
+const buildCipherStream = function buildCipherStream(password, salt) {
+    /**
+     * I want to put the IV in front of the encrypted data. So, when I start reading the stream,
+     * I have the IV and can decrypt via piping immediately.
+     */
+    class AppendInitializationVectorTransformer extends Transform {
+        constructor(initializationVector, opts) {
+            super(opts);
+            this.initializationVector = initializationVector;
+            this.appended = false;
+        }
 
-function decryptStream(key) {
-    if ( key.length !== 32 ) throw new Error('AES Key Must be 256 bytes (32 characters)');
-    return crypto.createDecipher(ALGORITHM, key);
-}
+        _transform(chunk, encoding, callback) {
+            if (!this.appended) {
+                this.push( Buffer.concat([ this.initializationVector, chunk ] ) );
+                this.appended = true;
+                return callback();
+            }
+            this.push(chunk);
+            callback();
+        }
+    }
+    const key = buildKey(password, salt);
+    let initializationVector = crypto.randomBytes(IV_LENGTH);
+    const ivStream = new AppendInitializationVectorTransformer(initializationVector);
+    const cipherStream = crypto.createCipheriv(ALGORITHM, key , initializationVector);
+    return { cipherStream, ivStream }
+};
 
-module.exports = { decrypt, encrypt, encryptStream, decryptStream };
+const buildDecipherStream = function buildDecipherStream( password, salt ) {
+    class DecipherTransformer extends Transform {
+        constructor(algorithm, key, opts) {
+            super(opts);
+            this.algorithm = algorithm;
+            this.key = key;
+            this.decipher = null;
+        }
+
+        _transform( chunk, encoding, callback ) {
+             if ( ! this.decipher ) {
+                 this.decipher = crypto.createDecipheriv( this.algorithm, this.key, chunk.slice( 0, IV_LENGTH ), null );
+                 chunk = chunk.slice( IV_LENGTH, chunk.length )
+            }
+            const decrypted = this.decipher.update(chunk);
+            this.push( Buffer.alloc(decrypted.length, decrypted) );
+            callback();
+        }
+        _flush(callback) {
+            if ( this.decipher ) {
+                this.push( this.decipher.final() );
+            }
+            callback();
+        }
+    }
+
+    const key = buildKey(password, salt);
+    return new DecipherTransformer( ALGORITHM, key, salt );
+};
+
+module.exports = { decrypt, encrypt, buildCipherStream, buildDecipherStream };
